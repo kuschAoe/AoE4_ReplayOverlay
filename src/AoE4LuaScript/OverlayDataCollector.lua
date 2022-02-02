c483321f4744e278255306c71ddf3b65b22dc6fc
kusch <kuchenschlachter@t-online.de>
2022-02-02 03:14:29 +0100

    population iinstead of entity count in lua

--- src/AoE4LuaScript/OverlayDataCollector.lua
+++ src/AoE4LuaScript/OverlayDataCollector.lua
@@ -7,21 +7,27 @@ function CollectData()
     for playerIndex = 1, playerCount do
         local player = World_GetPlayerAt(playerIndex)
         local playerName = Player_GetDisplayName(player).LocString;
-        local villagerCount = Player_GetEntityCountByUnitType(player, "worker")
         local team = Player_GetTeam(player)
         local civ = Player_GetRaceName(player)
 
         local egroup = Player_GetAllEntities(player)
         local entityCount = EGroup_Count(egroup)
-        local militaryUnitCount = 0
+        local militaryPopulation = 0
+        local workerPopulation = 0
+        local idleWorkerPopulation = 0
 
         for entityIndex=1, entityCount do
             local entity = EGroup_GetEntityAt(egroup, entityIndex)
-            if Entity_IsOfType(entity, "military") and not Entity_IsBuilding(entity) then
-                militaryUnitCount = militaryUnitCount + 1
-            --else
-                --local bp = Entity_GetBlueprint(entity)
-                --print(BP_GetName(bp))
+
+            if Entity_IsOfType(entity, "worker") then
+                local population = Entity_Population(entity, CT_Personnel)
+                workerPopulation = workerPopulation + population
+                local squad = Entity_GetSquad(entity)
+                if Squad_IsIdle(squad) then
+                    idleWorkerPopulation = idleWorkerPopulation + population
+                end
+            else if Entity_IsOfType(entity, "military") and not Entity_IsBuilding(entity) then
+                militaryPopulation = militaryPopulation + Entity_Population(entity, CT_Personnel)
             end
         end
 
@@ -30,9 +36,11 @@ function CollectData()
         table.insert(t, "\", \"civ\": \"")
         table.insert(t, civ)
         table.insert(t, "\", \"worker\": \"")
-        table.insert(t, tostring(villagerCount))
+        table.insert(t, tostring(workerPopulation))
+        table.insert(t, "\", \"idle\": \"")
+        table.insert(t, tostring(idleWorkerPopulation))
         table.insert(t, "\", \"military\": \"")
-        table.insert(t, tostring(militaryUnitCount))
+        table.insert(t, tostring(militaryPopulation))
         table.insert(t, "\", \"team\": \"")
         table.insert(t, tostring(team))
         table.insert(t, "\"}")
