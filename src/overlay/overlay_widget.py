5f3ab963460fbfe6167e383e17ed890bc066811f
kusch <kuchenschlachter@t-online.de>
2022-02-03 02:40:45 +0100

    font size resizes everything

--- src/overlay/overlay_widget.py
+++ src/overlay/overlay_widget.py
@@ -8,20 +8,13 @@ from overlay.custom_widgets import OverlayWidget
 from overlay.helper_func import file_path
 from overlay.settings import settings
 
-PIXMAP_CACHE = {}
-
-
 def set_pixmap(civ: str, widget: QtWidgets.QWidget):
     """ Sets civ pixmap to a widget. Handles caching."""
-    if civ in PIXMAP_CACHE:
-        widget.setPixmap(PIXMAP_CACHE[civ])
-        return
     path = file_path(f"img/{civ}")
     pixmap = QtGui.QPixmap(path)
     pixmap = pixmap.scaled(widget.width(), widget.height(), 
         QtCore.Qt.AspectRatioMode.KeepAspectRatio, 
         QtCore.Qt.TransformationMode.SmoothTransformation)
-    PIXMAP_CACHE[civ] = pixmap
     widget.setPixmap(pixmap)
 
 
@@ -29,8 +22,8 @@ class PlayerWidget:
     """ Player widget shown on the overlay"""
     def __init__(self, row: int, toplayout: QtWidgets.QGridLayout):
         self.visible = True
+        self.civ = "english"
         self.create_widgets()
-        self.name.setContentsMargins(5, 0, 0, 0)
 
         self.widgets = [self.flag, self.name, self.worker_icon, 
             self.worker, self.military_icon, self.military]
@@ -41,19 +34,18 @@ class PlayerWidget:
     def create_widgets(self):
         # Separated so this can be changed in a child inner overlay for editing
         self.flag = QtWidgets.QLabel()
-        self.flag.setFixedSize(QtCore.QSize(60, 50))
+        self.flag.setObjectName("flag")
         self.name = QtWidgets.QLabel()
         self.worker_icon = QtWidgets.QLabel()
         self.worker_icon.setObjectName("icon")
-        self.worker_icon.setFixedSize(QtCore.QSize(24, 48))
         set_pixmap("overlay_icons/worker.png", self.worker_icon)
         self.worker = QtWidgets.QLabel()
-        self.worker.setContentsMargins(0, 0, 10, 0)
+        self.worker.setAlignment(QtCore.Qt.Alignment(0x82))
         self.military_icon = QtWidgets.QLabel()
         self.military_icon.setObjectName("icon")
-        self.military_icon.setFixedSize(QtCore.QSize(24, 48))
         set_pixmap("overlay_icons/military.png", self.military_icon)
         self.military = QtWidgets.QLabel()
+        self.military.setAlignment(QtCore.Qt.Alignment(0x82))
 
     def show(self, show: bool = True):
         self.visible = show
@@ -66,7 +58,16 @@ class PlayerWidget:
         self.name.setStyleSheet("font-weight: bold; "
                                 f"color: rgba{color}")
 
+    def redraw_icons(self, font_size):
+        self.flag.setFixedSize(QtCore.QSize(font_size*4, font_size * 3))
+        set_pixmap("flags/" + self.civ + ".webp", self.flag)
+        self.worker_icon.setFixedSize(QtCore.QSize(font_size*9/4, font_size * 3))
+        set_pixmap("overlay_icons/worker.png", self.worker_icon)
+        self.military_icon.setFixedSize(QtCore.QSize(font_size*9/4, font_size * 3))
+        set_pixmap("overlay_icons/military.png", self.military_icon)
+
     def update_player(self, player_data: Dict[str, Any]):
+        self.civ = player_data['civ']
         set_pixmap("flags/" + player_data['civ'] + ".webp", self.flag)
         self.set_color(player_data['color'])
 
@@ -87,11 +88,10 @@ class AoEOverlay(OverlayWidget):
 
     def setup_as_overlay(self):
         if settings.overlay_geometry is None:
-            self.setGeometry(0, 0, 400, 150)
             sg = QtWidgets.QDesktopWidget().screenGeometry(0)
             self.move(sg.width() - self.width() + 15, sg.top() - 20)
         else:
-            self.setGeometry(*settings.overlay_geometry)
+            self.move(settings.overlay_geometry[0], settings.overlay_geometry[1])
 
         self.setWindowTitle('AoE IV: Overlay')
 
@@ -101,10 +101,9 @@ class AoEOverlay(OverlayWidget):
         self.playerlayout.setAlignment(QtCore.Qt.AlignRight
                                        | QtCore.Qt.AlignTop)
         self.setLayout(self.playerlayout)
-        self.update_style(settings.font_size)
-        
         # Add players
         self.init_players()
+        self.update_style(settings.font_size)
 
     def init_players(self):
         for i in range(8):
@@ -112,14 +111,19 @@ class AoEOverlay(OverlayWidget):
         [p.show(False) for p in self.players]
 
     def update_style(self, font_size: int):
-        self.setStyleSheet(
-            "OverlayWidget{background: black}"
-            f"QLabel {{font-size: {font_size}pt; color: white; font-weight: bold; margin-top: 15px}}"
-            "QLabel#icon {margin-top: 0px}"
-            )
+        [p.redraw_icons(font_size) for p in self.players]
 
+        self.setStyleSheet(
+            #"QWidget{background: red}"
+            "OverlayWidget{background: black}"
+            f"QLabel {{font-size: {font_size}pt; color: white; font-weight: bold; margin-top: {font_size*2/3}px; margin-right: {font_size*2/3}px; min-width: {font_size*4}px}}"
+            "QLabel#icon {margin-top: 0px; min-width: 0px; margin-right: 0px}"
+            "QLabel#flag {margin-top: 0px; min-width: 0px; margin-right: 0px}"
+            )
+        
         if self.isVisible():
             self.show()
+            self.setFixedSize(self.playerlayout.totalSizeHint())
 
     def update_data(self, game_data: Dict[str, Any]):
         if not self.fixed:
@@ -128,12 +132,10 @@ class AoEOverlay(OverlayWidget):
         [p.show(False) for p in self.players]
         for i, player in enumerate(game_data['players']):
             self.players[i].update_player(player)
-        self.setFixedSize(self.playerlayout.totalSizeHint())
 
     def save_geometry(self):
         """ Saves overlay geometry into settings"""
         pos = self.pos()
         settings.overlay_geometry = [
-            pos.x() + 8, pos.y() + 31, self.width(),
-            self.height()
+            pos.x() + 8, pos.y() + 31
         ]
